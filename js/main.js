5d4777c6a37c5c934edcc7fc5204df0391727392
Drew <drc920630@gmail.com>
2023-02-21 22:15:27 +0900

    섭메뉴 반응형으로 hide 추가 제거 완료 resize이벤트

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const snbBtn = document.querySelector(".snb .btn-menu");
 const subMenu = document.querySelector(".snb .sub-menu");
 
+
+// window.onresize = function(event){
+//   let winWidth = window.innerWidth;
+//   if (winWidth >= "969") {
+//     subMenu.classList.add("hide")
+//   }
+// }
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= "969") {
+    subMenu.classList.add("hide");
+  }
+});
+
+
+
 snbBtn.addEventListener("click", function () {
   if (subMenu.classList.contains("hide")) {
   subMenu.classList.remove("hide");
